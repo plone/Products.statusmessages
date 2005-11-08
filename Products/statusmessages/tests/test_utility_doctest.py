99c9e649f3e9f716b0c2f35f5146c0c5536b1c2f
Hanno Schlichting <hanno@hannosch.eu>
2005-11-08 17:49:32 +0000

    Initial import of statusmessages
    svn path=/statusmessages/trunk/; revision=13768

---
+++ Products/statusmessages/tests/test_utility_doctest.py
@@ -0,0 +1,15 @@
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from unittest import TestSuite
+from Testing.ZopeTestCase import ZopeDocTestSuite
+
+def test_suite():
+    return TestSuite((
+        ZopeDocTestSuite('Products.statusmessages.utility'),
+    ))
+
+if __name__ == '__main__':
+    framework()
+
