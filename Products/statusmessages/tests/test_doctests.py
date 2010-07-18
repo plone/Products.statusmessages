2ad81d2da47acea9357637def360504b1da83691
Hanno Schlichting <hanno@hannosch.eu>
2010-07-18 11:41:23 +0000

    Use the standard libraries doctest module.
    svn path=/statusmessages/trunk/; revision=121187

--- Products/statusmessages/tests/test_doctests.py
+++ Products/statusmessages/tests/test_doctests.py
@@ -1,16 +1,9 @@
-"""
-    Status messages tests.
-"""
-
-import unittest
+from doctest import DocTestSuite
 from unittest import TestSuite
-from zope.testing.doctestunit import DocTestSuite
+
 
 def test_suite():
     return TestSuite((
         DocTestSuite('Products.statusmessages.adapter'),
         DocTestSuite('Products.statusmessages.message'),
         ))
-
-if __name__ == '__main__':
-    unittest.main(defaultTest="test_suite")
