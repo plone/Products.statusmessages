a4b3bfa54fd08b0d2e89270393c111b25336b617
Maurits van Rees <maurits@vanrees.org>
2017-08-04 20:31:53 +0200

    Removed Plone 4.3 from classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
                       open('CHANGES.rst').read()),
     classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.3',
         'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
         'Framework :: Zope2',
