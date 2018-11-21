6d0159d309dbe44bb7d3b6508bd5b5d8a6685ad0
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-11-21 14:57:02 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='Zope CMF Plone status messages i18n',
     author='Hanno Schlichting',
